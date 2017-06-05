3872ecc62f5736a387f876617b46e59579260eb4
paulbhr <paulbh.roche@gmail.com>
2017-06-05 16:49:10 +0200

    maj 5617

--- script.js
+++ script.js
@@ -3,10 +3,36 @@ function scrollto(id) {
   element.scrollIntoView();
 }
 
-let slideIndex = 0;
-setInterval(function() {slideEntrees('entrees')}, 4000);
-setInterval(function() {slidePlats('plats')}, 6000);
-setInterval(function() {slideDesserts('desserts')}, 8000);
+
+window.onscroll = function() {scroll()};
+let initial = document.getElementById('logo').offsetTop;
+
+function scroll() {
+  if (window.scrollY >= initial) {
+    document.getElementById('logo').style.position = "fixed";
+    document.getElementById('logo').style.top = "0";
+    document.getElementById('logo').style.fontSize = "36px";
+    if (window.scrollY > document.getElementById('contact').offsetTop) {
+      document.getElementById('navbar').style.position = "static";
+    }
+    else {
+      document.getElementById('navbar').style.position = "fixed";
+    }
+  }
+  else {
+    document.getElementById('logo').style.position = "static";
+    document.getElementById('logo').style.fontSize = "72px";
+  }
+  console.log(document.getElementById('contact').offsetTop);
+  console.log(window.scrollY);
+}
+
+let slideE = 0;
+let slideP = 0;
+let slideD = 0;
+setInterval(function() {slideEntrees('entrees')}, 2000);
+setInterval(function() {slidePlats('plats')}, 2000);
+setInterval(function() {slideDesserts('desserts')}, 2000);
 
 function slideEntrees(x) {
     let container = document.getElementById(x).children;
@@ -14,9 +40,9 @@ function slideEntrees(x) {
     for (i = 0; i < container.length; i++) {
        container[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > container.length) {slideIndex = 1}
-    container[slideIndex-1].style.display = "block";
+    slideE++;
+    if (slideE > container.length) {slideE = 1}
+    container[slideE-1].style.display = "block";
 }
 
 function slidePlats(x) {
@@ -25,9 +51,9 @@ function slidePlats(x) {
     for (i = 0; i < container.length; i++) {
        container[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > container.length) {slideIndex = 1}
-    container[slideIndex-1].style.display = "block";
+    slideP++;
+    if (slideP > container.length) {slideP = 1}
+    container[slideP-1].style.display = "block";
 }
 
 function slideDesserts(x) {
@@ -36,7 +62,25 @@ function slideDesserts(x) {
     for (i = 0; i < container.length; i++) {
        container[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > container.length) {slideIndex = 1}
-    container[slideIndex-1].style.display = "block";
+    slideD++;
+    if (slideD > container.length) {slideD = 1}
+    container[slideD-1].style.display = "block";
 }
+
+function onScrollEvent(to_animate, func, number) {
+  $(window).scroll(function () {
+    if($(window).scrollTop() > to_animate.offset().top - number) {
+      setTimeout(func);
+    }
+  });
+}
+
+let child1 = $('.left');
+let child2 = $('.right');
+function func() {
+  child1.addClass('slideInDown');
+  child2.addClass('fadeInRight');
+
+}
+
+onScrollEvent(child2, func, 500);
